8f480b059ff08f5cc06f0fac774da4f19e7a75fd
Your Name <y.nkn.0628@gmail.com>
2021-07-26 20:07:04 +0900

    OAuthのや鯖に置いた

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect
 import os
 import json
+from requests_oauthlib import OAuth1Session
+import requests
+from urllib.parse import parse_qsl
 
 app = Flask(__name__, template_folder="./", static_url_path="")
 
@@ -8,9 +11,70 @@ Keys = json.load(open("keys.json", "r"))
 CK = Keys["CK"]
 CS = Keys["CS"]
 
+# URLs
+AccVerifyURL = "https://api.twitter.com/1.1/account/verify_credentials.json"
+UserTLURL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+ReqTokenURL = "https://api.twitter.com/oauth/request_token"
+AuthURL = "https://api.twitter.com/oauth/authenticate"
+
 @app.route('/')
 def hello():
     return render_template("deck.html")
+
+
+@app.route("/login")
+def login():
+    # Twitter Application Management で設定したコールバックURLsのどれか
+    oauth_callback = "{0}://{1}/oauth/".format(request.scheme, request.host)
+
+    twitter = OAuth1Session(CK, CS)
+
+    request_token_url = ReqTokenURL
+
+    response = twitter.post(
+    request_token_url,
+    params={'oauth_callback': oauth_callback}
+    )
+
+    # responseからリクエストトークンを取り出す
+    request_token = dict(parse_qsl(response.content.decode("utf-8")))
+
+    # リクエストトークンから連携画面のURLを生成
+    authenticate_url = AuthURL
+    authenticate_endpoint = "#"
+    try:
+        authenticate_endpoint = '%s?oauth_token=%s' % (authenticate_url, request_token['oauth_token'])
+    except KeyError:
+        authenticate_endpoint = request_token
+
+    print(authenticate_endpoint)
+    return redirect(authenticate_endpoint)
+
+
+@app.route("/oauth/")
+def oauth():
+
+    res = redirect("/")
+
+    oauthtoken = request.args["oauth_token"]
+    oauthverifier = request.args["oauth_verifier"]
+
+
+    twitter = OAuth1Session(CK, CS, oauthtoken, oauthverifier)
+
+    t = twitter.post("https://api.twitter.com/oauth/access_token", params={'oauth_verifier': oauthverifier})
+
+    print(t)
+
+    access_token = dict(parse_qsl(t.content.decode("utf-8")))
+
+    print(access_token)
+
+    res.set_cookie("at", access_token["oauth_token"])
+    res.set_cookie("as", access_token["oauth_token_secret"])
+
+    return res
+
 
 @app.route('/assets/<path:path>')
 def return_asset(path):
