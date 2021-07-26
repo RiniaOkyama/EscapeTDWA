1df89fd65c77dda163bf6953a8e8612e2b45e6ef
Your Name <y.nkn.0628@gmail.com>
2021-07-26 18:56:54 +0900

    ファイルのフォーマット+鯖側の調整+キーのサンプルファイル

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, send_from_directory
+import os
+import json
+
 app = Flask(__name__, template_folder="./", static_url_path="")
+
+Keys = json.load(open("keys.json", "r"))
+CK = Keys["CK"]
+CS = Keys["CS"]
 
 @app.route('/')
 def hello():
