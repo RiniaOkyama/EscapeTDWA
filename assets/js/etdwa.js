ba968990be70776127660ee6a553e629dff72120
hakunagi <hakunagi.rei@gmail.com>
2021-07-27 18:22:37 +0900

    etdwa.js追加・ローカル設定追加・ライトテーマ用意

---
+++ assets/js/etdwa.js
@@ -0,0 +1,31 @@
+const etdwa = {
+  localSettings: {
+    val: {
+      theme: 0,
+    },
+
+    set: function (name, val) {
+      this.val[name] = val
+      localStorage.setItem("localSettings", JSON.stringify(this.val))
+      this.commit()
+    },
+    load: function () {
+      localStorage.getItem("localSettings") === null
+        ? localStorage.setItem("localSettings", JSON.stringify(this.val))
+        : (this.val = JSON.parse(localStorage.getItem("localSettings")))
+      this.commit()
+    },
+    commit: function () {
+      Object.keys(this.val).forEach((e) => {
+        e === "theme" &&
+          (this.val[e] === 0
+            ? document.documentElement.classList.remove("light")
+            : document.documentElement.classList.add("light"))
+      })
+    },
+  },
+}
+
+window.onload = () => {
+  etdwa.localSettings.load()
+}
