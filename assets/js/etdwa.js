829d6fd326194e2a47474d968ec4b34b9a7898a7
hakunagi <hakunagi.rei@gmail.com>
2021-07-28 00:33:21 +0900

    webkit系のスクロールバーの見た目・テーマ切り替え用の仮ボタン

--- assets/js/etdwa.js
+++ assets/js/etdwa.js
@@ -19,11 +19,27 @@ const etdwa = {
       Object.keys(this.val).forEach((e) => {
         e === "theme" &&
           (this.val[e] === 0
-            ? document.documentElement.classList.remove("light")
-            : document.documentElement.classList.add("light"))
+            ? (document.documentElement.classList.remove("light"),
+              document.documentElement.classList.remove("black"))
+            : this.val[e] === 1
+            ? (document.documentElement.classList.add("light"),
+              document.documentElement.classList.remove("black"))
+            : (document.documentElement.classList.remove("light"),
+              document.documentElement.classList.add("black")))
       })
     },
   },
 }
 
 etdwa.localSettings.load()
+
+window.addEventListener("load", () => {
+  $("<a href='#' style='font-size:.7em'>テーマ(仮)</a>")
+    .click(() =>
+      etdwa.localSettings.set("theme", (etdwa.localSettings.val.theme + 1) % 3)
+    )
+    .appendTo(".sidebar_bottom_btns")
+  console.log(
+    "etdwa.jsでテーマ切り替えボタンを仮で置いてます\n切りたい場合は36~43行目をコメントアウトしてください"
+  )
+})
